840f2857ca41549f80390d699cb117374a9a1dbc
yogesh <yogesh.kumar1@magicbricks.com>
2021-11-27 17:27:15 +0530

    all test cases passed

--- assignments/run_callback/run_callback.js
+++ assignments/run_callback/run_callback.js
@@ -1,4 +1,8 @@
+
 function RunCallback(a, b, cb) {
+    
+    let m=(a+b);
+    return cb(m);
     // Write you code here, you need to add a and b, pass the result into callback function cb as argument return the result
 }
 
