d59b0fb21bc7515b9eafe70fc4119f9dd166e2b5
yogesh <yogesh.kumar1@magicbricks.com>
2021-11-29 15:03:11 +0530

    test cases passed

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,4 +1,13 @@
 function CreateObject(arr) {
+    const obj={};
+    for(let i=0;i<arr.length;i+=2){
+        obj[arr[i]]=arr[i+1];
+
+
+    }
+    return obj;
+
+
     // Write your code here
 }
 
